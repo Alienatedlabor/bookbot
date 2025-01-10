42c86d02de8e399bd4cc68b1f12fd8ac5d80456a
alienatedlabor <propertyisrobbery@hotmail.com>
2025-01-09 22:04:42 -0500

    add create report function

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count_book_words(text)
-    count_book_characters(text)
+    word_count = count_book_words(text)
+    character_dict = count_book_characters(text)
+    create_book_report(word_count, character_dict)
 
 # find book path and return entire text
 def get_book_text(path):
@@ -17,7 +18,6 @@ def count_book_words(text):
 
     for word in words:
       word_count += 1
-    print(f"The book has {word_count} words")
     return word_count
 
 # count the number of times each character appears in the string.
@@ -32,10 +32,26 @@ def count_book_characters(text):
       else: 
          character_dict[character] += 1
          
-    print(character_dict)
     return character_dict  
 
+def create_book_report(word_count, character_dict):
+    dict_list = []
+    def sort_on(dict_list):
+      return dict_list["num"]
+   
+    
 
+    #iterate through dictionary by key and value, append key and value to list if key is alphabetic. https://www.programiz.com/python-programming/methods/built-in/iter
+    for key, value in iter(character_dict.items()):
+        if key.isalpha() == True:
+            dict_list.append({"char": key, "num": value})
+      
+ 
+    dict_list.sort(reverse=True, key=sort_on)
+    for entry in dict_list:
+       print(f"The '{entry['char']}' character was found {entry['num']} times")
+
+    print(f"The book has {word_count} words")
 
 
 #call entry-point function

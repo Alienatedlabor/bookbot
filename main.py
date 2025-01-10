2db2650cf019ce2492affe51152d6c99c46bc25f
alienatedlabor <propertyisrobbery@hotmail.com>
2025-01-09 20:35:45 -0500

    add character count function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count_book_words(text)
+    count_book_characters(text)
 
 # find book path and return entire text
 def get_book_text(path):
@@ -18,6 +19,24 @@ def count_book_words(text):
       word_count += 1
     print(f"The book has {word_count} words")
     return word_count
+
+# count the number of times each character appears in the string.
+def count_book_characters(text):
+    character_dict = {}
+    lowered_string = text.lower()
+
+# loop through each character, add character to dict if not there, if there increment int value.
+    for character in lowered_string:
+      if character not in character_dict:
+        character_dict[character] = 1
+      else: 
+         character_dict[character] += 1
+         
+    print(character_dict)
+    return character_dict  
+
+
+
 
 #call entry-point function
 main()
